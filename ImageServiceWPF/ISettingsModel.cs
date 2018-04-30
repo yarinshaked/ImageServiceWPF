3f78cd1b608a989fd1484d588ad2984544868b6b
Yarin <yarin.shaked7@gmail.com>
2018-04-30 15:06:55 +0300

    first commit

---
+++ ImageServiceWPF/ISettingsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceWPF
+{
+    interface ISettingsModel
+    {
+        string ServerIP { get; set; }
+        int ServerPort { get; set; }
+        
+    }
+}
