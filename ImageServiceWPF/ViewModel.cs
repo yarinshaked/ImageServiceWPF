ad5d9e4b1b8bf932f9449777fa127cce0af3b676
Yarin <yarin.shaked7@gmail.com>
2018-05-01 17:21:39 +0300

    user control

--- ImageServiceWPF/ViewModel.cs
+++ ImageServiceWPF/ViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace ImageServiceWPF
 
         public void NotifyPropertyChanged(string propName)
         {
-            this.PropertyChanged?.Invoke(this, new PropetyChangedEventArgs(propName));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
 
     }
